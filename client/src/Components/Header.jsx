dcf2917633232578b7e2120092e48953d7414bd8
Colin Tolliver <colintolliver@gmail.com>
2023-12-19 09:25:09 -0800

    mobile friendly

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
 const Header = ({ onShowSignup, onShowLogin, onToggleFavorites }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+
   return (
     <header className="w-full bg-dark text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -9,7 +13,7 @@ const Header = ({ onShowSignup, onShowLogin, onToggleFavorites }) => {
             <img src="/logo.png" alt="Anime Atlas Logo" className="h-12 w-12" />
             <h1 className="text-3xl font-bold text-neon">Anime Atlas</h1>
           </div>
-          <div className="flex items-center space-x-4">
+          <div className="hidden sm:flex items-center space-x-4">
             <button
               className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-primary"
               onClick={onShowSignup}
@@ -33,9 +37,33 @@ const Header = ({ onShowSignup, onShowLogin, onToggleFavorites }) => {
             >
               Favorites
             </button>
-            <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-primary">
-              Categories
+          </div>
+
+          <div className="sm:hidden">
+            <button
+              tabIndex={0}
+              role="button"
+              className="btn m-1"
+              onClick={toggleDropdown}
+            >
+              Menu
             </button>
+            {isDropdownOpen && (
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a onClick={onShowSignup}>Sign Up</a>
+                </li>
+                <li>
+                  <a onClick={onShowLogin}>Login</a>
+                </li>
+                <li>
+                  <a onClick={onToggleFavorites}>Favorites</a>
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </div>
