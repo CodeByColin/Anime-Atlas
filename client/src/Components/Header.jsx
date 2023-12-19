f63bbbb1dc6b2e5b3372b4e6fec2d8de27390f1b
Colin Tolliver <colintolliver@gmail.com>
2023-12-19 10:21:12 -0800

    logo

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import logo from "/Users/colin/MCSP/Anime-Atlas/client/src/public/logo.png";
 
 const Header = ({ onShowSignup, onShowLogin, onToggleFavorites }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -10,11 +11,7 @@ const Header = ({ onShowSignup, onShowLogin, onToggleFavorites }) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-3">
           <div className="flex items-center space-x-1">
-            <img
-              src="https://files.oaiusercontent.com/file-zMKSBIFZzLH1jV9cyU8TL2on?se=2023-12-19T18%3A09%3A32Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D299%2C%20immutable&rscd=attachment%3B%20filename%3DDALL%25C2%25B7E%25202023-12-17%252015.52.07%2520-%2520Create%2520a%2520sleek%2520and%2520modern%2520logo%2520for%2520a%2520website%2520named%2520%2527Anime%2520Space%2527%2520that%2520incorporates%2520elements%2520of%2520anime%2520culture%2520and%2520the%2520concept%2520of%2520space.%2520The%2520logo%2520should.png&sig=Waf1fr6TE9Tss0Ka2Nyj3TU6JKEA4To3bWu5/Jq1iZ0%3D"
-              alt="Anime Atlas Logo"
-              className="h-12 w-12"
-            />
+            <img src={logo} alt="Anime Atlas Logo" className="h-12 w-12" />
             <h1 className="text-3xl font-bold text-neon">Anime Atlas</h1>
           </div>
           <div className="hidden sm:flex items-center space-x-4">
