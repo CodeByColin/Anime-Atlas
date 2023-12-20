03b35564819e34d60f21183e9054663da45be2b0
Colin Tolliver <colintolliver@gmail.com>
2023-12-19 16:19:58 -0800

    styling

--- client/src/Components/Favorites.jsx
+++ client/src/Components/Favorites.jsx
@@ -77,7 +77,7 @@ const Favorites = ({ showDetails }) => {
   return (
     <div className="overflow-y-auto py-8">
       <div className="text-center">
-        <h2 className="text-3xl font-bold mb-8">Your Favorites</h2>
+        <h2 className="text-3xl text-neon font-bold mb-8">Your Favorites</h2>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex flex-wrap -mx-2">
             {favorites.length > 0 ? (
